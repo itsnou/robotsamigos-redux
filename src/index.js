e9bc4c0b6e0e87d268fb4c651225ee82d766da3d
itsnou <emiliano.meneses@gmail.com>
2020-10-26 20:02:53 -0300

    redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider, connect } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
 import App from './contenedor/App';
 import 'tachyons';
+import { searchRobots } from './reducers'; 
 // import * as serviceWorker from './serviceWorker';
+
+const store= createStore(searchRobots)
 
 ReactDOM.render(
   <React.StrictMode>
